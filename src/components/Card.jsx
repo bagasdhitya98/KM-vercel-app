bba21f18416cbed95c45436dbb76fac54382a12a
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-10-02 15:07:10 +0700

    added feature news

---
+++ src/components/Card.jsx
@@ -0,0 +1,55 @@
+const Card = ({ id, title, image, author, date }) => {
+  const dateTimeList = date;
+  const dateTime = new Date(dateTimeList);
+
+  const weekList = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthList = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const dayOfWeek = weekList[dateTime.getUTCDay()];
+  const monthOfYear = monthList[dateTime.getUTCMonth()];
+  const day = dateTime.getUTCDate();
+  const year = dateTime.getUTCFullYear();
+
+  return (
+    <div id={id} className="w-full h-full rounded-md shadow-md">
+      <img
+        src={
+          image !== null && !image.includes("webp")
+            ? image
+            : "https://placehold.co/600x500"
+        }
+        className="w-full h-60"
+      />
+      <div className="p-4 flex flex-col gap-y-5">
+        <h2 className="text-black font-semibold">{title}</h2>
+        <p className="text-black">
+          Author : {author !== null ? author : "author not provided"}
+        </p>
+        <p className="text-black">{`${dayOfWeek} ,${day} ${monthOfYear} ${year}`}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
